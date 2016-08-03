9e004322ddf0a29bd0da84ad13844c6e5c1a6ad8
Marc Fournier <marc.fournier91@gmail.com>
2016-08-03 18:38:06 -0400

    Created migrations.

--- db/migrate/20160803222425_create_bars.rb
+++ db/migrate/20160803222425_create_bars.rb
@@ -1,6 +1,9 @@
 class CreateBars < ActiveRecord::Migration[5.0]
   def change
     create_table :bars do |t|
+      t.string :name
+      t.string :address
+      t.string :img_url
 
       t.timestamps
     end
--- db/migrate/20160803222501_create_happyhours.rb
+++ db/migrate/20160803222501_create_happyhours.rb
@@ -1,6 +1,9 @@
 class CreateHappyhours < ActiveRecord::Migration[5.0]
   def change
     create_table :happyhours do |t|
+      t.string :hours
+      t.string :weekday
+      t.references :bar, index:true, foreign_key: true
 
       t.timestamps
     end
