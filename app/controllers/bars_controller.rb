71277af9947dfc4906994e39a89288ccb5daf40c
Marc Fournier <marc.fournier91@gmail.com>
2016-08-03 19:59:52 -0400

    Adds create and edit functionality for bars.

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -8,4 +8,34 @@ class BarsController < ApplicationController
     @bar = Bar.find(params[:id])
   end
 
+  def new
+    @bar = Bar.new
+  end
+
+  def edit
+    @bar = Bar.find(params[:id])
+  end
+
+  def create
+    @bar = Bar.new bar_params
+    if @bar.save
+      redirect_to @bar
+    end
+  end
+
+  def update
+    @bar = Bar.find(params[:id])
+
+    if @bar.update(bar_params)
+      redirect_to @bar
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def bar_params
+    params.require(:bar).permit(:name, :address, :img_url)
+  end
+
 end
