b23f72a600decf5c7bc62a6b3443ed9ddf102f54
Marc Fournier <marc.fournier91@gmail.com>
2016-08-04 19:11:02 -0400

    Adds create functionality

--- app/controllers/happyhours_controller.rb
+++ app/controllers/happyhours_controller.rb
@@ -4,4 +4,21 @@ class HappyhoursController < ApplicationController
     @happyhours = @bar.happyhours
   end
 
+  def new
+    @bar = Bar.find(params[:bar_id])
+    @happyhour = @bar.happyhours.build
+  end
+
+  def create
+    @bar = Bar.find(params[:bar_id])
+    @happyhour = @bar.happyhours.create!(happyhour_params)
+
+    redirect_to bar_happyhours_path(@bar)
+  end
+
+  private
+  def happyhour_params
+    params.require(:happyhour).permit(:hours, :weekday)
+  end
+
 end
