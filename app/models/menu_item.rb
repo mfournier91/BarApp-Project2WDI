9e3b7920b80da2e39a17fe0e1b809cbd1d877965
Marc Fournier <marc.fournier91@gmail.com>
2016-08-05 12:58:22 -0400

    Puts price validation in the model where it belongs.

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,5 @@
 class MenuItem < ApplicationRecord
   belongs_to :happyhour
+  validates :name, presence: true
+  validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0, :less_than => 10}
 end
--- db/migrate/20160805152942_create_menu_items.rb
+++ db/migrate/20160805152942_create_menu_items.rb
@@ -2,7 +2,7 @@ class CreateMenuItems < ActiveRecord::Migration[5.0]
   def change
     create_table :menu_items do |t|
       t.string :name
-      t.string :price validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0, :less_than => 10}
+      t.string :price
       t.references :happyhour, index:true, foreign_key: true
 
       t.timestamps
