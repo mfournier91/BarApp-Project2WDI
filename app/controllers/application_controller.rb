1f153e324a084c13965b9c5830b6094957d35857
Marc Fournier <marc.fournier91@gmail.com>
2016-08-07 16:19:16 -0400

    Gives users ability to sign in/out/up with links.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
   before_filter :authenticate_user!, :except => [:index, :show]
+
+private
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
