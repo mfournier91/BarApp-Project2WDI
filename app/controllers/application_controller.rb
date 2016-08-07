c25e5aefcdcbb103ea8c3445902a41fb5e39700d
Marc Fournier <marc.fournier91@gmail.com>
2016-08-07 15:45:59 -0400

    Adds authentication for edit create and delete routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # before_action :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index, :show]
 end
