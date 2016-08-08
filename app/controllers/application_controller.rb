507fedd6ca389e6b92b33df9728845bc90b6b124
Marc Fournier <marc.fournier91@gmail.com>
2016-08-08 12:36:43 -0400

    Fixes problem with last commit. Lets users update their registration.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
   before_filter :authenticate_user!, :except => [:index, :show]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-private
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+
   end
+
+# private
+#   def after_sign_out_path_for(resource_or_scope)
+#     root_path
+#   end
+
 end
