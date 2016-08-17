c7cf6cb9493455e63c796253f398359a9bdfdb10
Marc Fournier <marc.fournier91@gmail.com>
2016-08-17 19:21:54 -0400

    Trying stuff for bgimg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.serve_static_assets = true
-  config.assets.compile = true
+  config.serve_static_assets = false
+  config.assets.compile = false
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
