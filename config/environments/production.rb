fc723b6713773e22831f2c34b1c912d9671a3289
Marc Fournier <marc.fournier91@gmail.com>
2016-08-17 19:22:17 -0400

    jk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.serve_static_assets = false
-  config.assets.compile = false
-  
+  config.serve_static_assets = true
+  config.assets.compile = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
